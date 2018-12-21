5c3da889e5f78e4606c848ef14d851d31f8aea1a
Richard Conway <richard.conway@telespazio.com>
2018-12-21 09:36:03 +0000

    Added some comments to 'fileserver'.

--- fileserver/main.go
+++ fileserver/main.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
+// Listening address parts.
 var hostname = "0.0.0.0" // or could use e.g. ""
 var port = 8080
 
+// Serve the index.html file contents.
 func serveIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-// NOT USED - but here is a most basic example of how to serve all files from the root dir down.
+// FUNC NOT USED - but here is a most basic example of how to serve all files from the root dir down.
 func serveRootDir() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 }
 
+// Serve all files under the 'public' directory, stripping off the '/public' prefix so they appear
+// as if they are located from the root.
 func servePublicDir() http.Handler {
 	return http.StripPrefix("/public/", http.FileServer(http.Dir("./public")))
 }
@@ -25,12 +29,15 @@ func servePublicDir() http.Handler {
 func main() {
 	fmt.Println("FileServer started...")
 
+	// Routing
 	http.HandleFunc("/", serveIndex)
 	http.Handle("/public/", servePublicDir())
 
+	// Start listening
 	listenAddress := fmt.Sprintf("%s:%d", hostname, port)
 	fmt.Printf("Listening on address: '%s'\n", listenAddress)
 	log.Fatal(http.ListenAndServe(listenAddress, nil))
 
+	// This will never get executed
 	fmt.Println("...FileServer completed.")
 }
